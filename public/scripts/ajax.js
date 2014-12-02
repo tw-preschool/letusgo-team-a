e03882c4ab45e1d1941ebf662dc516efe594a9d1
Zyy <1070948611@qq.com>
2014-12-02 19:24:02 +0800

    delete useless additems.rb and modify app.rb ajax.rb spec_helper.rb

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -12,4 +12,15 @@
 			type: 'GET'
 	});
 })();
-
+$.ajax({
+	url:"/products",
+	success:function(data){
+		$(data).each(function(index,product){
+			var add = '<button class = "btn btn-sm-success">加入购物车</button>';
+			var row = '<tr><td>' + product.name + '</td><td>' + product.price + '</td><td>' +
+				  product.unit + '</td><td>' + add + '</td></tr>';
+			$('#item-table').append(row);		
+		});
+	},
+	type:"GET"
+});
