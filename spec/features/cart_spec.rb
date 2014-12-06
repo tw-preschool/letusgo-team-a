ae55d0028daf8c874a9f0d76f9c9a4fb3ae95954
SanCoder-Q <sancoder.q@gmail.com>
2014-12-06 23:19:08 +0800

    [#010][SanCoder] - add first scenario of cart test

---
+++ spec/features/cart_spec.rb
@@ -0,0 +1,41 @@
+#encoding=UTF-8
+require_relative '../spec_helper'
+
+feature 'shopping cart page' do
+
+    background :each do
+        items = []
+        items.push :name => '可口可乐', :unit => '瓶', :price => 3.00, :promotion => false
+        items.push :name => '雪碧', :unit => '瓶', :price => 3.00, :promotion => true
+        items.push :name => '苹果', :unit => '斤', :price => 5.50, :promotion => false
+        items.push :name => '荔枝', :unit => '斤', :price => 15.00, :promotion => false
+        items.push :name => '电池', :unit => '个', :price => 2.00, :promotion => false
+        items.push :name => '方便面', :unit => '袋', :price => 4.50, :promotion => true
+        items.each do |item|
+            Product.create(item).to_json
+        end
+    end
+
+    scenario "should get correct shopping list and price when a user add 3瓶雪碧 and 3斤苹果 in cart and visit '/cart'", js: true do
+        visit '/views/items.html'
+        (1..3).each do page.find(:xpath, '//button[../../td[contains(.,"雪碧")]]').click end
+        (1..3).each do page.find(:xpath, '//button[../../td[contains(.,"苹果")]]').click end
+        visit '/cart'
+        expect(page).to have_selector(:xpath, '//td[../td[contains(.,"雪碧")]][contains(.,"6.00")]')
+        expect(page).to have_selector(:xpath, '//td[../td[contains(.,"苹果")]][contains(.,"16.50")]')
+        expect(page).to have_selector(:xpath, '//p[./span[contains(.,"总计：")]][contains(.,"22.50")]')
+    end
+
+    scenario "should get correct shopping list and price when a user continully add 3瓶雪碧 in nonempty cart and visit '/cart'", js: true do
+        # given
+    end
+
+    scenario "cart should be empty when a user close browser then back to visit '/cart'", js: true do
+        # given
+    end
+
+    scenario "sum price should change correspondingly when a user visit '/cart' and change the amount of 苹果 in nonempty cart", js: true do
+        # given
+    end
+
+end
