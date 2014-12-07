d38f7de595ea5b5854fd3d95d7aac703bfd3a746
Lin Liu <linliu@thoughtworks.com>
2014-12-07 18:09:49 +0800

    [010][Liu Lin] - use localStorage to store itemCount and count

--- public/scripts/model/item.js
+++ public/scripts/model/item.js
@@ -1,6 +1,32 @@
-function Item(barcode, name, unit, price) {
-    this.barcode = barcode;
+var Item = function(id, name, unit, price, count) {
+    this.id = id;
     this.name = name;
     this.unit = unit;
     this.price = price || 0.00;
+    this.count = count || 1;
+}
+
+Item.prototype.isPromoted = function () {
+  if (this.count < 2) {
+    return false;
+  }
+
+  // var id = this.id;
+  // var promotions = loadPromotions().filter(function(promotion) {
+  //   return promotion.ids.contains(id);
+  // });
+
+  return promotions && promotions.length ? true : false;
+}
+
+Item.prototype.realPrice = function() {
+  return (this.price * this.realCount()).toFixed(2);
+}
+
+Item.prototype.realCount = function() {
+  return this.isPromoted() ? (Math.ceil(this.count / 2)) : this.count;
+}
+
+Array.prototype.contains = function(id) {
+  return this.indexOf(id) > -1;
 }
--- public/scripts/product.js
+++ public/scripts/product.js
@@ -1,5 +1,55 @@
 $(document).ready(function () {
-	
 	// $('#addProduct').on('click', addProduct);
+	loadProducts();
 
+	function addProduct() {
+		$.ajax({
+	        url: '/products',
+	        data: {
+	            name: $('#productName').val(),
+	            price: $('#productPrice').val(),
+	            unit: $('#productUnit').val()
+	        },
+	        success: function(data) {
+	        	appendProductByUrl(data.message);
+	        },
+	        type: 'POST'
+	    });
+	}
+
+	function appendProductByUrl(url) {
+		$.ajax({
+			url: url,
+			success: function(data){
+				appendProduct(data);
+			},
+			type: 'GET'
+		});
+	}
+
+	function loadProducts() {
+		$.ajax({
+			url: '/products',
+			success: function(data) {
+				$(data).each(function(index, value) {
+					appendProduct(value);
+				});
+			},
+			type: 'GET'
+		});
+	}
+
+	function appendProduct(product) {
+		var addButton = '<button class="btn btn-sm btn-success addCart">加入购物车</button>';
+		var row = $('<tr><td>' + product.name + '</td><td>' + product.price + '</td><td>' + product.unit + '</td><td>' + addButton + '</td></tr>');
+
+		$('button', row).click(function() {
+			cartStorage.setCount('count');
+			$('#count').text(cartStorage.getCount('count'));
+
+			cartStorage.setItemCount((product.id).toString());
+		});
+
+		$('#item-table').append(row);
+	}
 });
