12c67bd2b2b5314b632caee4bbf8335e9b736e76
Zyy <1070948611@qq.com>
2014-12-08 13:36:46 +0800

    fix the id dependency of promotion

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -3,14 +3,13 @@
 		url: '/products',
 		success: function(data) {
 			$(data).each(function(index, product) {
-				var s = index + 1;
-				var check = product.promotion? "checked":"";//开关由"checked"决定
+				var check = product.promotion? "checked":"";				//开关由"checked"决定
 				var add = '<button class="btn btn-sm btn-success addCart">加入购物车</button>';
 				var promotion = '<div class="bootstrap-switch bootstrap-switch-small" id='+product.id+'><input type="checkbox" name="my-checkbox" 				 		'+check+'></div>';
 				var row = '<tr><td>' + product.name + '</td><td>' + product.price + '</td><td>' +
 					product.unit + '</td><td>' + add + '</td></tr>';
 				$('.item-table').append(row);
-				$("#inline-promotion" + s ).append(promotion);
+				$("#inline-promotion" + product.id ).append(promotion);
 				$("#"+product.id).on('click',{msg:product.id},isPromotion);
 			});
 			$("[name='my-checkbox']").bootstrapSwitch();
