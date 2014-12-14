d74d19812ae61a6249f3f586eedd9445dcd1da48
Lin Liu <linliu@thoughtworks.com>
2014-12-14 13:06:38 +0800

    [#009][Liu Lin] - valid productStock in the add product page

--- public/scripts/count.js
+++ public/scripts/count.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
 
-  var count = sessionStorage.getItem('count') || 0;
-  $('#count').text(count);
+  	$("[name='productStock']").on('focusout', checkProductStock);
+  	$('#addProduct').on('click', addProduct);
 
+  	var count = sessionStorage.getItem('count') || 0;
+  	$('#count').text(count);
+
+  	function isProductStockValid() {
+  		return $("[name='productStock']").val() >= 0;
+  	}
+  
+	function checkProductStock() {
+  		if(!isProductStockValid()) {
+  			$('#errorTip').text('库存必须大于等于0').show();
+    	} else {
+    		$('#errorTip').text('库存必须大于等于0').hide();
+    	}
+  	}
+
+  	function addProduct() {
+  		if(!isProductStockValid()) {
+  			$('#errorTip').text('库存必须大于等于0');
+  			return false;
+    	} else {
+    		return true;
+    	}
+  	}
 });
