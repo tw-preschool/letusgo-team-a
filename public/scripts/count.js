dd497669b616f4c3cac71362fbf6e681e5b1d746
Zyy <1070948611@qq.com>
2014-12-15 20:01:47 +0800

    fix stockValidation problem

--- public/scripts/count.js
+++ public/scripts/count.js
@@ -1,24 +1,6 @@
 $(document).ready(function(){
 
-  	$("[name='productStock']").on('focusout', checkProductStock);
-  	$('#addProduct').on('click', checkProductStock);
-  	$('#editProduct').on('click', checkProductStock);
-
   	var count = sessionStorage.getItem('count') || 0;
   	$('#count').text(count);
-
-  	function isProductStockValid() {
-  		return $("[name='productStock']").val() >= 0;
-  	}
-  
-	function checkProductStock() {
-  		if(!isProductStockValid()) {
-  			$('#errorTip').text('库存必须大于等于0').show();
-  			return false;
-    	} else {
-    		$('#errorTip').hide();
-    		return true;
-    	}
-  	}
 
 });
--- public/scripts/operation.js
+++ public/scripts/operation.js
@@ -13,10 +13,29 @@ function open() {
   $("#hidden").fadeIn();
   var div = $("#modify").html();
   $("#bg").html(div);
+  $("[name='productStock']").focusout(checkProductStock);
+  $("#editProduct").click(checkProductStock);
 }
 
 function add() {
   $("#hidden").fadeIn();
   var div = $("#add").html();
   $("#bg").html(div);
+  $("[name='productStock']").focusout(checkProductStock);
+  $("#addProduct").click(checkProductStock);
 }
+
+function isProductStockValid() {
+  		return $("[name='productStock']").val() >= 0;
+}
+  
+function checkProductStock() {
+  	if(!isProductStockValid()) {
+		$('#errorTip').attr('style','color:red');
+  		$('#errorTip').text('库存必须大于等于0').show();
+  		return false;
+    	} else {
+    		$('#errorTip').hide();
+    		return true;
+    		}
+ }
