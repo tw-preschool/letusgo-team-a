7f69d08e4a4eb70de0283971ea09110d1041dda4
zlienqwe <zhu303490@gmail.com>
2014-12-15 20:26:15 +0800

    zj:add price validation

--- public/scripts/operation.js
+++ public/scripts/operation.js
@@ -14,7 +14,9 @@ function open() {
   var div = $("#modify").html();
   $("#bg").html(div);
   $("[name='productStock']").focusout(checkProductStock);
+  $("[name='productPrice']").focusout(checkProductPrice);
   $("#editProduct").click(checkProductStock);
+  $("#editProduct").click(checkProductPrice);
 }
 
 function add() {
@@ -22,20 +24,37 @@ function add() {
   var div = $("#add").html();
   $("#bg").html(div);
   $("[name='productStock']").focusout(checkProductStock);
+  $("[name='productPrice']").focusout(checkProductPrice);
   $("#addProduct").click(checkProductStock);
+  $("#addProduct").click(checkProductPrice);
 }
 
 function isProductStockValid() {
-  		return $("[name='productStock']").val() >= 0;
+      return $("[name='productStock']").val() >= 0;
+}
+
+function isProductPriceValid() {
+      return $("[name='productPrice']").val() >= 0;
 }
   
 function checkProductStock() {
   	if(!isProductStockValid()) {
-		$('#errorTip').attr('style','color:red');
-  		$('#errorTip').text('库存必须大于等于0').show();
+		$('#stockErrorTip').attr('style','color:red');
+  		$('#stockErrorTip').text('库存必须大于等于0').show();
   		return false;
     	} else {
-    		$('#errorTip').hide();
+    		$('#stockErrorTip').hide();
     		return true;
     		}
+ }
+
+ function checkProductPrice() {
+    if(!isProductPriceValid()) {
+    $('#priceErrorTip').attr('style','color:red');
+      $('#priceErrorTip').text('价格必须大于等于0').show();
+      return false;
+      } else {
+        $('#priceErrorTip').hide();
+        return true;
+        }
  }
