63ecf0740fe059769ebc950c729e3d68cd0ba25b
Zyy <1070948611@qq.com>
2014-12-18 11:08:19 +0800

    add 注销提醒 in cart.erb

--- public/scripts/count.js
+++ public/scripts/count.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
 
   	var count = sessionStorage.getItem('count') || 0;
   	$('#count').text(count);
-
 });
+function state() {
+  var a = $("#state").text();
+    alert(a);
+  if(a == "登录") {
+    return true;
+  }
+  else {
+    return confirm(are you sure?);
+  }
+}
